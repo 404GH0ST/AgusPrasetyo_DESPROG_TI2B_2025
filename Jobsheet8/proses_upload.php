93396c4646904f2ebf5c865182489ee798b386d7
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2025-11-01 20:51:28 +0700

    Jobsheet 8

---
+++ Jobsheet8/proses_upload.php
@@ -0,0 +1,34 @@
+<?php
+// Lokasi penyimpanan file yang diunggah
+$targetDirectory = "images/";
+
+// Periksa apakah direktori penyimpanan ada, jika tidak maka buat
+if (!file_exists($targetDirectory)) {
+    mkdir($targetDirectory, 0777, true);
+}
+
+if (isset($_FILES['files']['name'][0])) {
+    $totalFiles = count($_FILES['files']['name']);
+
+    // Loop melalui semua file yang diunggah
+    for ($i = 0; $i < $totalFiles; $i++) {
+        $fileName = $_FILES['files']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
+        if (in_array($fileType, $allowedExtensions)) {
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+                echo "File $fileName berhasil diunggah.<br>";
+            } else {
+                echo "Gagal mengunggah file $fileName.<br>";
+            }
+        } else {
+            echo "File $fileName tidak valid (hanya gambar yang diizinkan).<br>";
+        }
+    }
+} else {
+    echo "Tidak ada file yang diunggah.";
+}
+?>
